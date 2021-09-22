f0c692ab6fedf803d1f6b0c821f338dab91e50bc
edmund-hsd <edmundhang@ymail.com>
2021-09-22 10:00:05 +0700

    email validation

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,5 @@
 class Passenger < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   belongs_to :booking
 end
